ea418902cf36c440191f2de0ba0ab5c71ff36d54
David Klingenberg <dklingenberg@gmail.com>
2024-07-30 00:03:23 +0200

    feat: Add PWA Support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { ExerciseListComponent } from './exercise-list/exercise-list.component';
 import { ExerciseDetailComponent } from './exercise-detail/exercise-detail.component';
 import { TrainingComponent } from './training/training.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,13 @@ import { TrainingComponent } from './training/training.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
