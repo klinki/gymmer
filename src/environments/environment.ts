f9acae5679371b27b4db31090c91422e55c00cec
David Klingenberg <dklingenberg@gmail.com>
2026-01-22 20:46:48 +0100

    chore: Update .gitignore and env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // Run 'npx dexie-cloud create' to get your database URL
-  dexieCloudUrl: 'https://<YOUR_DB_ID>.dexie.cloud',
+  dexieCloudUrl: 'https://z5n728bfs.dexie.cloud',
 };
